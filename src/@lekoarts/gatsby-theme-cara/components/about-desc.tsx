a226c82006e09f61eece07d36c3b7b7ad3d60a2a
Stefano Romani <fanoromani@gmail.com>
2023-09-10 22:04:34 -0300

    intro and about

--- src/@lekoarts/gatsby-theme-cara/components/about-desc.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/about-desc.tsx
@@ -41,12 +41,12 @@ const AboutDesc = () => (
         },
       }}
     >
-      You know the way you feel when you see a picture of two otters holding
-      hands? That's how you're gonna feel every day. My mother cried the day I
-      was born because she knew she'd never be prettier than me. You should make
-      me your campaign manager. I was born for politics. I have great hair and I
-      love lying. Captain? The kids want to know where Paulie the Pigeon is. I
-      told them he got sucked up into an airplane engine, is that all right?
+      I'm deeply committed to crafting exceptional web experiences that marry
+      world-class design with flawless functionality. I'm passionate about
+      staying up-to-date with the latest frontend trends and technologies. I
+      thrive on technical challenges and am always on the lookout for new
+      opportunities to learn and grow. Let's connect and collaborate on amazing
+      projects!
     </p>
   </div>
 );
